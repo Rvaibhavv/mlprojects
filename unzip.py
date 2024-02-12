aba5eca9fa6664428bff16faa8d186c7b23cc390
Rvaibhavv <rvaibhav615@gmail.com>
2024-02-13 01:17:22 +0530

    unzipper portable

--- unzip.py
+++ unzip.py
@@ -1,9 +1,12 @@
 import zipfile
 import os
-path_of_zip ="C:/Users/rvaib/Downloads/titanic.zip"
-name_of_dataset ="titanic1"
 
-unzip_path = f"C:/Users/rvaib/onedrive/desktop/mlprojects/datasets/{name_of_dataset}"
-os.makedirs(unzip_path,exist_ok=True)
-with zipfile.ZipFile(path_of_zip, 'r') as zip_ref:
-    zip_ref.extractall(unzip_path)
+
+def dataunzip(path,name):
+    path =f"C:/Users/rvaib/Downloads/{path}"
+    unzip_path = f"C:/Users/rvaib/onedrive/desktop/mlprojects/datasets/{name}"
+    os.makedirs(unzip_path,exist_ok=True)
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        zip_ref.extractall(unzip_path)
+
+
